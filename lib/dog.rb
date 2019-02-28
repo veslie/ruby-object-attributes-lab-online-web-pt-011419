477f9e71fb81fa38f552356e637a54852a682924
veslie losius <veslielosius@yahoo.com>
2019-02-28 22:34:34 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,5 @@
-class Dog
-  def name=(dog_name)
-    this_dogs_name = dog_name
+ class Dog
+   
+ def initialize(name)
+    @name = name 
   end
- 
-  def name
-    this_dogs_name
-  end
-end
- 
- 
